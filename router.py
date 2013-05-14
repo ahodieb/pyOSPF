744163eb44d820c364d859a311706e8dc2e6f8d5
The Night Phoenix <abdallah.hodieb@gmail.com>
2013-05-14 20:54:49 +0200

    updated router for more thread safty

--- router.py
+++ router.py
@@ -1,74 +1,99 @@
 
 from threading import Thread
+import threading
 import time
 import os
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='(%(threadName)-10s) %(message)s', )
 
 
 class router_thread(Thread):
 
-    def __init__(self, buffer_writer):
+    def __init__(self, router, name, lock):
         Thread.__init__(self)
         self.on = True
-        self.buffer = buffer_writer.buffer
-
-    def run(self):
-        while(self.on):
-            time.sleep(1)
-            self.buffer.append(self.getName() + time.strftime('    {%H:%M:%S}'))
-
-
-class buffer_writer_thread(Thread):
-
-    def __init__(self, file_name):
-        Thread.__init__(self)
         self.buffer = []
-        self.buffer_write_interval = 5
-        self.file_name = file_name
-        self.on = True
+        self.lock = lock
+        self.file_name = 'routers/' + name + '.txt'
+        self.router = router
 
-    def run(self):
-        while(self.on):
-            time.sleep(self.buffer_write_interval)
-            if self.buffer:  # to prevent opening the file if there is nothing to write.
+    def dump(self):
+        # locking the thread to write affects the performance !
+        logging.debug('Dumping buffer to files.')
+        logging.debug(self.buffer)
+
+        with self.lock:
+            if self.buffer:
                 with open(self.file_name, 'a+b') as f:
                     while self.buffer:
                         f.write(self.buffer.pop(0) + '\n')
 
+    def write(self, s):
+        logging.debug('writing to output buffer.')
+        with self.lock:
+            self.buffer.append(s + '      ' + time.strftime('{%H:%M:%S}'))
+            logging.debug(s)
+
+    def run(self):
+        while(self.on):
+            time.sleep(1)
+            logging.debug('Router Started')
+            self.write('hi')
+            time.sleep(1)
+            self.write('hello')
+            time.sleep(2)
+            self.dump()
+            # self.buffer.append(self.getName() + time.strftime('
+            # {%H:%M:%S}'))
+
 
 class router(object):
 
-    def __init__(self, area, name, priority=1, connections=[], ):
+    def __init__(self, area, name, lock, priority=1, connections=[], ):
         self.priority = priority
         self.connections = connections
         self.area = area
         self.DR = self
         self.name = name
+        self.connections = {}
 
-        self.buffer_writer = buffer_writer_thread('routers/' + self.name + '.txt')
-        self.router_thread = router_thread(self.buffer_writer)
+        self.router_thread = router_thread(router=self, name=name, lock=lock)
         self.router_thread.name = self.name
 
-    def network_connect(self, router):
-        self.connections.append(router)
-        router.connections.append(self)
+    def connect(self, connection):
+        self.connections[connection['ip']] = connection
+
+    def setup_connection(router, ip, connection_type, NIC, note=''):
+        return {'router': router, 'ip': ip, 'connection_type': connection_type, 'NIC': NIC, 'note': note}
 
 
 def main():
     print 'PID ', os.getpid()
 
-    r1 = router(0, 'r1')
-    r2 = router(0, 'r2')
-    r3 = router(0, 'r3')
-    r1.network_connect(r2)
-    r1.network_connect(r3)
+    lock = threading.RLock()
 
-    r1.buffer_writer.start()
+    r1 = router(area=0, name='r1', lock=lock)
+    r2 = router(area=0, name='r2', lock=lock)
+    r3 = router(area=0, name='r3', lock=lock)
+
+    logging.debug("Created Routers")
+
+    logging.debug(router.setup_connection(
+        r2, '192.168.0.4', 'eth', '90:E6:BA:88:18:3A'))
+
+    r1.connect(router.setup_connection(
+        r2, '192.168.0.4', 'eth', '90:E6:BA:88:18:3A'))
+
+    r1.connect(router.setup_connection(
+        r3, '192.168.0.4', 'eth', '00:02:21:04:43:21'))
+
+    logging.debug("Coneected Routers")
+
     r1.router_thread.start()
-
-    r2.buffer_writer.start()
     r2.router_thread.start()
-
-    r3.buffer_writer.start()
     r3.router_thread.start()
 
 if __name__ == '__main__':
