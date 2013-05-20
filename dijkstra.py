385711d4d01cad948b401f2b6fa3364f159870d3
Abdallah Hodieb <abdallah.hodieb@gmail.com>
2013-05-21 00:52:11 +0200

    fixed a weird bug ! , i don't know how i did that shit ! and how it didnt show in testing

--- dijkstra.py
+++ dijkstra.py
@@ -5,7 +5,7 @@ def dijkestra(G, O):
     S = [O]  # shoretes path nodes
     V = G.keys()  # Vertices
     V.remove(O)
-    
+
     d = {O: 0}  # distance
     E = {}  # un optimized routing table . need to adjuest gatways
 
@@ -18,8 +18,15 @@ def dijkestra(G, O):
         Ep[v] = (v, O)
 
     while (V):
+        print V
+        print d
+        print dp
+        
+
 
         for v in V:
+            print '-'*20
+            print v
         # this loop is ineffecient try to optimise later (23/4/2013)
         # causes quadratic time atleast
         # i already cash the results in the above variables but i cant get
@@ -28,12 +35,18 @@ def dijkestra(G, O):
             #     if d[s] + G[s][v] < dp[v]:
             #         dp[v] = d[s] + G[s][v]
             #         Ep[v] = (v, s)
+            print d[n_s]
+            print G[n_s][v]
+            print dp[v]
 
             if d[n_s] + G[n_s][v] < dp[v]:
                 dp[v] = d[n_s] + G[n_s][v]
                 Ep[v] = (v, n_s)
+            print '-'*20
 
-        m = min(dp)
+        m = min(dp,key=dp.get)
+        print dp
+        print m
 
         V.remove(m)
         S.append(m)
@@ -44,6 +57,8 @@ def dijkestra(G, O):
 
         E[m] = (Ep[m])
         Ep.pop(m)
+
+        print "*"*20
 
     return (d, E)
 
@@ -83,16 +98,24 @@ def main():
 # """
     inf = float("inf")
     G = {
-        1: {1: 0, 2: 4, 3: 6, 4: 8, 5: inf, 6: inf, 7: inf},
-        2: {1: inf, 2: 0, 3: 1, 4: inf, 5: 7, 6: inf, 7: inf},
-        3: {1: inf, 2: inf, 3: 0, 4: 2, 5: 5, 6: 4, 7: inf},
-        4: {1: inf, 2: inf, 3: inf, 4: 0, 5: inf, 6: 5, 7: inf},
-        5: {1: inf, 2: inf, 3: inf, 4: inf, 5: 0, 6: inf, 7: 6},
-        6: {1: inf, 2: inf, 3: inf, 4: inf, 5: 1, 6: inf, 7: inf},
-        7: {1: inf, 2: inf, 3: inf, 4: inf, 5: inf, 6: inf, 7: inf},
+        1: {1: 0,   2: 4,   3: 6,   4: 8,   5: inf, 6: inf, 7: inf},
+        2: {1: inf, 2: 0,   3: 1,   4: inf, 5: 7,   6: inf, 7: inf},
+        3: {1: inf, 2: inf, 3: 0,   4: 2,   5: 5,   6: 4,   7: inf},
+        4: {1: inf, 2: inf, 3: inf, 4: 0,   5: inf, 6: 5,   7: inf},
+        5: {1: inf, 2: inf, 3: inf, 4: inf, 5: 0,   6: inf, 7: 6},
+        6: {1: inf, 2: inf, 3: inf, 4: inf, 5: 1,   6: inf, 7: inf},
+        7: {1: inf, 2: inf, 3: inf, 4: inf, 5: inf, 6: inf, 7: 0},
     }
 
-    d, E = dijkestra(G, 1)
+    G = {
+        'r4': {'r4': 0, 'r5': inf, 'r1': inf, 'r2': 10, 'r3': 25},
+        'r5': {'r4': inf, 'r5': 0, 'r1': inf, 'r2': 10, 'r3': inf},
+        'r1': {'r4': inf, 'r5': inf, 'r1': 0, 'r2': 100, 'r3': 100},
+        'r2': {'r4': 10, 'r5': 10, 'r1': 100, 'r2': 0, 'r3': 50},
+        'r3': {'r4': 25, 'r5': inf, 'r1': 100, 'r2': 50, 'r3': 0}
+    }
+
+    d, E = dijkestra(G, 'r5')
     print d
     print E
 
