1cdacbfda80b5bf578942710e800c99d4ce662a8
Abdallah Hodieb <abdallah.hodieb@gmail.com>
2013-05-20 20:46:37 +0200

    neigbours detection almost done. needs some refinment

--- router.py
+++ router.py
@@ -21,36 +21,41 @@ class router_thread(Thread):
         self.router.buffer.write(s)
 
     def run(self):
-        logging.debug('Router Started')
+        logging.debug('Router Thread Started Started')
+
         while(self.on):
             time.sleep(2)
 
-            for c in self.router.connections.values():
-                logging.debug('sending to : ' + c['router'].name)
-                c['router'].send_hello(
+            for N, r in self.router.physical_connections.items():
+
+                logging.debug('sending to : ' + r.name)
+
+                local_config = self.router.NIC_config[N]
+                r.send_hello(
 
                     {
                         #'version':'',
                         #'type':'hello',
                         #'packet_lenght':'',
 
-                        'router_id': '',
-                        'area_id': '',
+                        'router_id': local_config['ip'],
+                        'area_id': local_config['area'],
 
                         'Au_type': '0',  # no auth
                         #'checksum':'',
 
-                        'network_mask': '255.255.255.0',
-                        'router_priority': '1',
+                        'network_mask': local_config['sn_mask'],
+                        'router_priority': self.router.priority,
 
                         'hello_interval': '10',
                         'dead_interval': '30',
 
                         'DR': '',
                         'BDR': '',
-                        'Neighbour': self.router.connections.keys(),
+                        'Neighbour': self.router.neigbours.values(),
                         'options': {}
-                    }
+                    },
+                    self.router
 
                 )
 
@@ -130,7 +135,7 @@ class router(object):
         if pair:
             r.connect_physical(self, rni, lni, False)
 
-    def send_hello(self, p):
+    def send_hello(self, packet, sender):
         """
         {
         ##header##
@@ -156,7 +161,20 @@ class router(object):
         }
         """
         self.router_thread.write(
-            'Hello Recived ' + time.strftime('{%H:%M:%S}') + str(p) + '\n')
+            'Hello Recived ' + time.strftime('{%H:%M:%S}') + str(packet) + '\n')
+
+        # check if same subnet
+        # there are more checks than this
+        # like hello intevral and dead intveraval
+        # that need to mach but im not doing them for now at least .
+
+        for N, r in self.physical_connections.items():
+            if r.name == sender.name:
+                if self.NIC_config[N]['sn_mask'] == packet['network_mask']:
+                    self.neigbours[packet['router_id']] = N
+                    self.router_thread.write(
+                        'Neighbour Added' + time.strftime('{%H:%M:%S} ') + packet['router_id'] + ' ' + sender.name + '\n')
+                break
 
 
 def main():
@@ -204,11 +222,19 @@ def main():
 
     logging.debug("Coneected Routers")
 
-    # r1.router_thread.start()
-    # r2.router_thread.start()
+    r1.buffer.start()
+    r2.buffer.start()
+    r3.buffer.start()
+    r4.buffer.start()
+
+    r1.router_thread.start()
+    r2.router_thread.start()
     # r3.router_thread.start()
+    # r4.router_thread.start()
 
 
 if __name__ == '__main__':
     main()
     raw_input()
+    raw_input()
+    os._exit(1)
